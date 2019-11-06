f213ae91bf9ab23b30e3888e29ee670d9890e7a0
charlie lai <47859948+dkrcharlie@users.noreply.github.com>
2019-11-06 13:42:59 +0800

    user annotation like @PreAuthorize,@Secured to protect API

--- src/main/java/com/dkrcharlie/demo/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/dkrcharlie/demo/springsecurity/config/SecurityConfiguration.java
@@ -1,42 +1,31 @@
 package com.dkrcharlie.demo.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .roles("user")
-                .and()
-                .withUser("admin")
-                .password("password")
-                .roles("admin");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("admin")
-                .antMatchers("/user").hasAnyRole("admin","user")
-                .antMatchers("/").permitAll()
-                .and()
-                .formLogin()
-                .and()
-                .logout();
-    }
-
+public class SecurityConfiguration {
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails manager = User.withDefaultPasswordEncoder()
+                .username("manager")
+                .password("password")
+                .roles("MANGER")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin,manager);
     }
 }
